6658646c0cb9fca51626b8c725be0f072d63778d
vienthe.mai <vienthe.mai@Admins-MacBook-Pro-5.local>
2023-09-12 17:08:50 +0700

    update{

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SongModule } from './song/song.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'sql6.freemysqlhosting.net',
-      port: 3306,
-      username: 'sql6641988',
-      password: 'H7eZmi43Vs',
-      database: 'sql6641988',
-      autoLoadEntities: true,
-    }),
-    SongModule,
-  ],
+  // imports: [
+  //   TypeOrmModule.forRoot({
+  //     type: 'mysql',
+  //     host: 'sql6.freemysqlhosting.net',
+  //     port: 3306,
+  //     username: 'sql6641988',
+  //     password: 'H7eZmi43Vs',
+  //     database: 'sql6641988',
+  //     autoLoadEntities: true,
+  //   }),
+  //   SongModule,
+  // ],
   controllers: [AppController],
   providers: [AppService],
 })
